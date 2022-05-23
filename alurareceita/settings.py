dd7877e8184a3543d66821a110ca148ca67b8fb5
leonardofguedes <leonardo.fguedes@gmail.com>
2022-05-23 20:19:35 -0300

    Commit

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
